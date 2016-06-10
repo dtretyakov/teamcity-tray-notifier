ea46bf8e601bc9e4a67e356c8a69f1d2593afce9
Igor Shevchenko <cyberskunk@gmail.com>
2016-06-10 11:12:45 +0300

    store server URL in config file

--- desktop/server-url.js
+++ desktop/server-url.js
@@ -1,15 +1,34 @@
+const { app } = require('electron');
 const PARAMNAME = 'TEAMCITY_TRAY_NOTIFIER_URL';
-let serverURL;
+const CONFIG_FILE = '.ttn.json';
+const fs = require('fs');
+const path = require('path');
+
+let serverURL = null;
+let confifFilePath = path.join(app.getPath('appData'), app.getName(), CONFIG_FILE);
+
+// console.log(path.join(app.getPath('appData'), app.getName()));
+
 
 module.exports = {
 
     getServerUrl: function () {
         serverURL = process.env[PARAMNAME];
+        if (!serverURL) {
+            try {
+                let config = require(confifFilePath);
+                serverURL = config.serverURL;
+            } catch (e) {
+                // TODO
+            }
+        }
         return serverURL;
     },
 
     saveServerUrl: function (serverURL) {
-        process.env[PARAMNAME] = serverURL;
+        fs.writeFileSync(confifFilePath, JSON.stringify({
+            serverURL: serverURL
+        }), 'utf8');
     }
 
 };
