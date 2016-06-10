3aa71f863088ebe6e04b3e3208513f19b7fa0eb1
Dmitry.Tretyakov <dtretyakov@gmail.com>
2016-06-10 11:36:39 +0300

    Add icon to builds window and change default size

--- desktop/builds/builds.js
+++ desktop/builds/builds.js
@@ -1,16 +1,20 @@
 let {
     BrowserWindow
 } = require('electron');
+const path = require('path');
 
 let buildsWin = null;
 
 module.exports = function(serverURL) {
+    const iconPath = path.join(__dirname,'../icon.png');
     buildsWin = new BrowserWindow({
-        height: 150,
+        width: 500,
+        height: 300,
         webPreferences: {
             nodeIntegration: false
         },
-        frame: false
+        frame: false,
+        icon: iconPath
     });
 
     buildsWin.loadURL(`${serverURL}trayNotifier/viewBuilds.html`);
