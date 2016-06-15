8dce16f275603a16c6ec3fc4a9dab6691ad5674a
Igor Shevchenko <cyberskunk@gmail.com>
2016-06-15 18:54:26 +0300

    improve server url validation

--- desktop/server-config/server-config-browser.js
+++ desktop/server-config/server-config-browser.js
@@ -2,6 +2,7 @@ const ipc = require('electron').ipcRenderer;
 let btn = document.querySelector('#btn');
 let closeButton = document.querySelector('#close');
 let serverUrlInput = document.querySelector('#server-url');
+let serverUrlLabel = document.querySelector('.server-url__label');
 
 btn.disabled = !serverUrlInput.validity.valid;
 
@@ -9,30 +10,46 @@ btn.addEventListener('click', () => {
     ipc.send('server-url-updated', serverUrlInput.value);
 });
 
-var dbg = document.querySelector('.debug');
+// let dbg = document.querySelector('.debug');
+let inputHandler = function () {
+    if (serverUrlInput.validity.valid || serverUrlInput.validity.customError) {
+        validateURL(serverUrlInput.value);
+    }
 
-serverUrlInput.addEventListener('input', function() {
     btn.disabled = !serverUrlInput.validity.valid;
-    dbg.innerHTML = serverUrlInput.validity.valid;
-    dbg.innerHTML += validateURL(serverUrlInput.value);
-});
+    serverUrlLabel.innerText = serverUrlInput.validationMessage;
+};
+
+serverUrlInput.addEventListener('input', inputHandler);
 
 closeButton.addEventListener('click', () => {
     ipc.send('close-configuration-window');
 });
 
 function validateURL(url) {
-    var parser = document.createElement('a');
+    let parser = document.createElement('a');
+    let isValid = false;
     try {
         parser.href = url;
-        return !!parser.hostname;
+
+        if (!(parser.protocol && /^https?:$/i.test(parser.protocol))) {
+            isValid = false;
+            serverUrlInput.setCustomValidity('"http(s):" protocol is required');
+        } else {
+            isValid = true;
+            serverUrlInput.setCustomValidity('');
+        }
+        return ;
     } catch (e) {
-        return false;
+        isValid = false;
     }
+
+    return isValid;
 }
 
-ipc.on('got-url', (e, url, sender) => {
+ipc.on('got-url', (e, url) => {
     serverUrlInput.value = url;
+    inputHandler();
 });
 
 serverUrlInput.focus();
